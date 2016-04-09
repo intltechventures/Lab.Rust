a10e32b1720c9474878d600c84a3f87757bee34d
Kelvin D. Meeks <kmeeks@intltechventures.com>
2016-04-08 17:57:28 -0700

    addeding README placeholders, and created directories for languages

---
+++ benchmarks/src/pb_make_dirs.cmd
@@ -0,0 +1,38 @@
+
+mkdir Clojure
+touch Closure\README.md 
+
+mkdir Go
+touch Go\README.md
+
+mkdir Groovy
+touch Groovy\README.md
+
+mkdir Java
+touch Java\README.md
+
+mkdir Javascript
+touch Javascript\README.md
+
+mkdir Julia
+touch Julia\README.md
+
+mkdir PHP
+touch PHP\README.md
+
+mkdir Python
+touch Python\README.md
+
+mkdir R
+touch R\README.md
+
+mkdir Ruby
+touch Ruby\README.md
+
+mkdir Rust
+touch Rust\README.md
+
+mkdir Scala
+touch Scala\README.md
+
+
